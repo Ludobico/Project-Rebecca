2048d38e1d26cd4b53a40f3ca77861287a648717
Ludobico <aqs450@gmail.com>
2023-01-27 09:41:25 +0900

    gltf optimization

--- src/Components/ThreeCanvas/GltfModel.jsx
+++ src/Components/ThreeCanvas/GltfModel.jsx
@@ -11,13 +11,13 @@ import { GLTFLoader } from "three-stdlib";
 import LoaderPro from "./LoaderPro";
 
 const GltfModel = () => {
-  const gltf = useLoader(GLTFLoader, "/nightcity/scene.gltf");
-  // gltf.materials.Dtla_Orbit_u1_v1.wireframe = true;
+  const gltf = useLoader(GLTFLoader, "/nc_optim/scene.gltf");
+  gltf.materials.Dtla_Orbit_u1_v1.wireframe = true;
   // gltf.materials.Dtla_Orbit_u1_v2.wireframe = true;
   // gltf.materials.Dtla_Orbit_u2_v1.wireframe = true;
-  // gltf.materials.Dtla_Orbit_u2_v2.wireframe = true;
-  // gltf.materials.Dtla_Orbit_u3_v1.wireframe = true;
-
+  gltf.materials.Dtla_Orbit_u2_v2.wireframe = true;
+  gltf.materials.Dtla_Orbit_u3_v1.wireframe = true;
+  console.log(gltf);
   return (
     <>
       <primitive object={gltf.scene} scale={1.2} />;
