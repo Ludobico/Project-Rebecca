48975ad24c1f93103e4c597bc0c5f2806560c1f8
Ludobico <aqs450@gmail.com>
2023-01-27 14:28:35 +0900

    wireframe apply

--- src/Components/ThreeCanvas/GltfModel.jsx
+++ src/Components/ThreeCanvas/GltfModel.jsx
@@ -13,8 +13,8 @@ import LoaderPro from "./LoaderPro";
 const GltfModel = () => {
   const gltf = useLoader(GLTFLoader, "/nc_optim/scene.gltf");
   gltf.materials.Dtla_Orbit_u1_v1.wireframe = true;
-  // gltf.materials.Dtla_Orbit_u1_v2.wireframe = true;
-  // gltf.materials.Dtla_Orbit_u2_v1.wireframe = true;
+  gltf.materials.Dtla_Orbit_u1_v2.wireframe = true;
+  gltf.materials.Dtla_Orbit_u2_v1.wireframe = true;
   gltf.materials.Dtla_Orbit_u2_v2.wireframe = true;
   gltf.materials.Dtla_Orbit_u3_v1.wireframe = true;
   console.log(gltf);
@@ -22,7 +22,7 @@ const GltfModel = () => {
     <>
       <primitive object={gltf.scene} scale={1.2} />;
       <EffectComposer multisampling={0}>
-        <BrightnessContrast brightness={0} contrast={0.5} />
+        <BrightnessContrast brightness={0.1} contrast={0.5} />
         <SMAA />
       </EffectComposer>
     </>
